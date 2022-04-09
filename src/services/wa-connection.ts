5dd981efaa12c5bf438e3978bf8f95429bb0fa39
José Rodolfo Valério Costa <rdfvalerio@gmail.com>
2022-04-08 21:33:03 -0300

    feat: configurações no controller

--- src/services/wa-connection.ts
+++ src/services/wa-connection.ts
@@ -1,46 +1,59 @@
 import { Client, LocalAuth, MessageContent } from "whatsapp-web.js";
-const qrcode = require('qrcode-terminal');
+const qrcode = require("qrcode-terminal");
 
 const waConfig = { puppeteer: { headless: false }, authStrategy: new LocalAuth() };
 
 export class WAConection {
     private client: Client;
-    
+
     constructor() {
         this.client = new Client(waConfig);
     }
 
     execute() {
-        this.client.on('qr', (qr) => {
-            console.log('QR Code:', qr);
+        this.client.on("qr", (qr) => {
+            console.log("QR Code:", qr);
             qrcode.generate(qr, { small: true });
         });
 
         this.client.initialize();
 
-        this.client.on('authenticated', () => {
-            console.log('Autenticado!');
+        this.client.on("authenticated", () => {
+            console.log("Autenticado!");
         });
 
-        this.client.on('auth_failure', msg => {
-            console.error('Falha na autenticação.', msg);
+        this.client.on("auth_failure", msg => {
+            console.error("Falha na autenticação.", msg);
         });
 
-        this.client.on('ready', () => {
-            console.log('Cliente logado com sucesso!');
+        this.client.on("ready", () => {
+            console.log("Cliente logado com sucesso!");
         });
 
-        this.client.on('message', async msg => {
-            if (msg.body === '!ping') {
+        this.client.on("message", async msg => {
+            if (msg.body === "!ping") {
                 let chat = await msg.getChat();
                 console.log(chat)
-                msg.reply('pong!');
+                msg.reply("pong!");
             }
         });
     }
 
-    sendMsg(msg: MessageContent) {
-        this.client.sendMessage("558396324407-1411859188@g.us", msg)
+    sendMsg(address: string, msg: MessageContent) {
+        const time = new Date().toLocaleTimeString();
+        this.client.sendMessage(address, msg)
+        console.log(`Nova mensagem enviada [${time}]: ${msg}`);
+    }
+
+    sendPong() {
+        this.client.on("message", async msg => {
+            if (msg.body === "!ping") {
+                msg.reply("pong");
+            } if (msg.body === "!status") {
+                let chat = await msg.getChat();
+                console.table(chat);
+            }
+        });
     }
 }
 
--- src/use-case/controller.ts
+++ src/use-case/controller.ts
@@ -1,6 +1,10 @@
 import { Webpages } from "../data/webpages";
 import { Scores } from "../services/score-scraping";
 import { WAConection } from "../services/wa-connection";
+import dotenv from "dotenv";
+
+dotenv.config();
+const timer = Number(process.env.TIMER) * 60000;
 
 export class Controller {
     private inProgress: string[] = [];
@@ -8,12 +12,28 @@ export class Controller {
 
     constructor(private waConnection: WAConection, private score: Scores) { }
 
-    execute() {
-        // this.waConnection.execute();
+    async execute() {
+        this.waConnection.execute();
+
+        await this.score.execute(Webpages.pageScores, "current");
+        await this.score.execute(Webpages.pageGameCloses, "closed");
+        console.log("Primeira verificação realizada com sucesso.");
 
         const getData = setInterval(async () => {
-            this.inProgress = await this.score.execute(Webpages.pageScores);
-            console.log(this.inProgress);
-        }, 5000);
+            const time = new Date().toLocaleTimeString();
+            console.log(`Buscando novas informações... ${time}`);
+            this.inProgress = await this.score.execute(Webpages.pageScores, "current");
+            this.closed = await this.score.execute(Webpages.pageGameCloses, "closed");
+
+            if (this.inProgress.length || this.closed.length) {
+                this.waConnection.sendMsg(process.env.CGRC as string, this.inProgress.join("\n"));
+                this.waConnection.sendMsg(process.env.CGRC as string, this.closed.join("\n"));
+                this.inProgress = [];
+                this.closed = [];
+            } else {
+                console.log("Nenhuma atualização encontrada.")
+            }
+
+        }, timer);
     }
 }
